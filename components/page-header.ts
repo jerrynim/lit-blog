9514c0ebf83a610bcd3fdb68968af9a036861560
jerrynim <tjerry3@naver.com>
2021-06-06 17:58:58 +0900

    lit-post에서 이동

--- components/page-header.ts
+++ components/page-header.ts
@@ -1,15 +1,24 @@
-import { LitElement, html } from "lit";
+import { LitElement, html, css } from "lit";
 import { customElement } from "lit/decorators.js";
 import { resetCss } from "@styles";
 import logoIcon from "/logo.svg?raw";
 
 @customElement("page-header")
 export class PageHeader extends LitElement {
-    static styles = [resetCss];
+    static styles = [
+        resetCss,
+        css`
+            :host {
+                display: flex;
+                height: 60px;
+                align-items: center;
+            }
+        `,
+    ];
 
     protected render() {
         const LogoIcon = html([logoIcon] as any);
-        return html` <lit-link href="/">${LogoIcon}</lit-link> `;
+        return html`<lit-link href="/">${LogoIcon}</lit-link>`;
     }
 }
 
