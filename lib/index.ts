972e47f1066c7c53dca921064e1d741323eafd8d
jerrynim <tjerry3@naver.com>
2021-10-04 14:13:29 +0900

    parseDate 수정

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,7 @@
 export const parseDate = (date: Date) => {
-    return `${date.getFullYear()}-${("0" + (date.getMonth() + 1)).slice(
-        -2,
-    )}-${date.getDate()}`;
+    return `${date.getFullYear()}-${("0" + (date.getMonth() + 1)).slice(-2)}-${(
+        "0" + date.getDate()
+    ).slice(-2)}`;
 };
 
 export const resetMetaData = () => {
