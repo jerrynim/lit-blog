06954ab936ec544166909c168437babc85fd8389
jerrynim <tjerry3@naver.com>
2021-12-14 23:25:23 +0900

    set head

--- pages/post/async-await.ts
+++ pages/post/async-await.ts
@@ -9,9 +9,9 @@ export class AsyncAwait extends LitElement {
     protected render() {
         return html`<post-head-image src=''></post-head-image>
            <post-head>
-               <h1></h1>
-               <post-tag></post-tag>
-               <post-date></post-date>
+               <h1>async await </h1>
+               <post-tag>javascript async await promise</post-tag>
+               <post-date>2021-12-14</post-date>
            </post-head>
            <post-body></post-body>`;
     }
