683b30e2a9c801d41e60fbfa0632a093d40dc57a
jerrynim <tjerry3@naver.com>
2021-12-28 18:30:24 +0900

    안드로이드 웹소켓 url 에러 2

--- pages/post/android-websocket-url-error.ts
+++ pages/post/android-websocket-url-error.ts
@@ -3,22 +3,34 @@ import { customElement } from "lit/decorators/custom-element.js";
 import { withPost } from "@lib/decorators";
 import "@components/post";
 
-@customElement("android-websocket-urll-error")
+@customElement("android-websocket-url-error")
 @withPost
 export class AndroidWebSocketUrlError extends LitElement {
     protected render() {
-        return html`<post-head-image src=""></post-head-image>
+        return html`<post-head-image
+                src="https://res.cloudinary.com/dij9kacx9/image/upload/v1640683677/lit-blog/semi_qqxojj.jpg"
+            ></post-head-image>
             <post-head>
-                <h1></h1>
-                <post-tag></post-tag>
-                <post-date></post-date>
+                <h1>
+                    Expected URL scheme 'http' or 'https' but no colon was found
+                </h1>
+                <post-tag>android websocket no colon error</post-tag>
+                <post-date>2021-12-28</post-date>
             </post-head>
-            <post-body></post-body>`;
+            <post-body
+                >You can solve this problem just add semicolon(;) at the end of
+                url
+                <post-image
+                    src="https://res.cloudinary.com/dij9kacx9/image/upload/v1640683677/lit-blog/%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A3%E1%86%BA_2021-12-28_%E1%84%8B%E1%85%A9%E1%84%92%E1%85%AE_6.03.47_gj5vze.png"
+                    alt="Expected URL scheme 'http' or 'https' but no colon was found"
+                    width="500"
+                ></post-image>
+            </post-body>`;
     }
 }
 
 declare global {
     interface HTMLElementTagNameMap {
-        "android-websocket-urll-error": AndroidWebSocketUrlError;
+        "android-websocket-url-error": AndroidWebSocketUrlError;
     }
 }
